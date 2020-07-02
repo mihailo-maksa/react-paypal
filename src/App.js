d1e64611bf8cd84ebb1559f721e0544069b7f21e
Mihajlo Maksa <mihajlomaksa9@gmail.com>
2020-07-02 18:52:52 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [paidFor, setPaidFor] = useState(false);
+  const [error, setError] = useState(null);
+
+  const product = {
+    name: 'Fidget Spinner',
+    price: 9.99,
+    img:
+      'https://3gstore.rs/p/120/120029/fidget-spinner-crveni-106287-47631-95063.png',
+  };
+
+  let paypalRef = useRef();
+
+  useEffect(() => {
+    window.paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: product.name,
+                amount: {
+                  currency_code: 'USD',
+                  value: product.price,
+                },
+              },
+            ],
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          setPaidFor(true);
+          console.log({ data, order });
+        },
+        onError: (err) => {
+          setError(err);
+          console.error(err);
+        },
+      })
+      .render(paypalRef.current);
+  }, [product.name, product.price]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {paidFor ? (
+        <div>
+          <h1>Thank You for Buying {product.name} from Us!</h1>
+          <img
+            src='https://image.freepik.com/free-vector/thank-you-lettering-with-curls_1262-6964.jpg'
+            alt='Thank You'
+          />
+        </div>
+      ) : (
+        <div>
+          {error && <span>Uh oh, an error occurred! {error.message}</span>}
+          <h1>
+            Buy {product.name} for ${product.price}
+          </h1>
+          <img src={product.img} alt={product.name} width='200' />
+          <div ref={paypalRef} />
+        </div>
+      )}
     </div>
   );
 }
